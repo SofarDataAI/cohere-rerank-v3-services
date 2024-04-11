5c09b09c09e204b5e2d6c5d0c708cb757327bc36
KennyDizi <xamarindevelopervietnam@outlook.com>
2024-04-12 06:29:58 +0700

    Init project CDK

---
+++ test/cohere-rerank-v3-services.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CohereRerankV3Services from '../lib/cohere-rerank-v3-services-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cohere-rerank-v3-services-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CohereRerankV3Services.CohereRerankV3ServicesStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
