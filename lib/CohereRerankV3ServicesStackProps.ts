927c4c352f0806323a2288e9987618a34f05537a
KennyDizi <xamarindevelopervietnam@outlook.com>
2024-04-12 19:04:37 +0700

    Add CohereRerankV3ServicesStackProps inteface

---
+++ lib/CohereRerankV3ServicesStackProps.ts
@@ -0,0 +1,70 @@
+import { StackProps } from "aws-cdk-lib";
+
+export interface CohereRerankV3ServicesStackProps extends StackProps, CohereRerankV3Services, CohereRerankV3ServiceArgs {
+}
+
+export interface CohereRerankV3Services {
+    /**
+     * The name of the application.
+     */
+    readonly appName: string;
+    /**
+     * Prefix used for naming AWS resources.
+     */
+    readonly resourcePrefix: string;
+    /**
+     * The version of the Docker image.
+     */
+    readonly ecrRepositoryImageTag: string;
+    /**
+     * The name of the Dockerfile.
+     */
+    readonly dockerfileName: string;
+    /**
+     * The name of the ECR repository.
+     */
+    readonly ecrRepositoryName: string;
+    /**
+     * The environment where the CDK stack will be deployed.
+     */
+    readonly cdkDeployEnvironment: string;
+    /**
+     * The platform for the deployment.
+     */
+    readonly cdkDeployPlatform: string;
+    /**
+     * The port number for the application.
+     */
+    readonly cdkDeployPort: string;
+    /**
+     * The AWS region where the stack will be deployed.
+     */
+    readonly cdkDeployRegion: string;
+    /**
+     * The string representation of the platform for the deployment.
+     */
+    readonly cdkDeployPlatformString: string;
+    /**
+     * The cpu type for the deployment.
+     */
+    readonly cdkDeployCpuType: string;
+    /**
+     * The memory type for the deployment.
+     */
+    readonly cdkDeployMemoryType: string;
+}
+
+export interface CohereRerankV3ServiceArgs {
+    /**
+     * The API key for Cohere services.
+     */
+    readonly cohereApiKey: string;
+    /**
+     * The model for Cohere services.
+     */
+    readonly cohereEmbedModel: string;
+    /**
+     * The API key for data ingestion services.
+     */
+    readonly dataIngestionApiKey: string;
+}
