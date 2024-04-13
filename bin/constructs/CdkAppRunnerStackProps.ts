abb42e021ecbde451101c7915d04ebe4b9254b71
KennyDizi <xamarindevelopervietnam@outlook.com>
2024-04-13 07:49:54 +0700

    Add CdkCohereRerankV3AppRunnerStackProps

---
+++ bin/constructs/CdkAppRunnerStackProps.ts
@@ -0,0 +1,29 @@
+import { NestedStackProps } from "aws-cdk-lib";
+import * as cdk from "aws-cdk-lib";
+import { CohereRerankV3Services } from "../../lib/CohereRerankV3ServicesStackProps";
+
+export interface CdkCohereRerankV3AppRunnerStackProps extends NestedStackProps, CohereRerankV3Services {
+    /**
+     * The VPC where the database should be deployed.
+     */
+    readonly vpc: cdk.aws_ec2.IVpc;
+    /**
+     * The ECR repository
+     */
+    readonly ecrRepository: cdk.aws_ecr.Repository;
+    /**
+     * The App Runner docker run arguments.
+     */
+    readonly dockerRunArgs: CdkCohereRerankV3DockerRunArgTyped,
+}
+
+export interface CdkCohereRerankV3DockerRunArgTyped {
+    /**
+     * The API key for Cohere services.
+     */
+    readonly COHERE_API_KEY: string;
+    /**
+     * The model for Cohere services.
+     */
+    readonly COHERE_EMBED_MODEL: string;
+}
