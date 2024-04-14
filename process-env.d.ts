b2470f50a03f3fc02f05de2159b8ff832ac8d6a4
KennyDizi <xamarindevelopervietnam@outlook.com>
2024-04-14 18:31:12 +0700

    Add process-env.d.ts

---
+++ process-env.d.ts
@@ -0,0 +1,17 @@
+declare module NodeJS {
+    interface ProcessEnv {
+        [key: string]: string | undefined;
+        CDK_DEPLOY_REGION: string;
+        ENVIRONMENT: string;
+        ECR_REPOSITORY_NAME: string;
+        APP_NAME: string;
+        IMAGE_VERSION: string;
+        DOCKERFILE_NAME: string;
+        PLATFORM: string;
+        PORT: string | undefined;
+        COHERE_API_KEY: string | undefined;
+        COHERE_RERANK_MODEL: string | undefined;
+        CPU_TYPE: string | undefined;
+        MEMORY_TYPE: string | undefined;
+    }
+}
